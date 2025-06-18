15fdd1f972f417014a856c437668699b287f48f1
DreNogue <andrenogueira619@gmail.com>
2025-06-18 12:04:19 -0300

    Proteção contra loop infinito

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ function sortear(){
     
     let sorteados = [];
     let numero;
-    
-      if (de >= ate) {
+
+    if (de >= ate) {
         alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
         return;
+    }
+
+    if (quantidade > (ate - de + 1)) {
+    alert('Campo "Quantidade" deve ser menor ou igual ao intervalo informado no campo "Do número" até o campo "Até o número". Verifique!');
+    return;
     }
 
     for (let i = 0; i < quantidade; i++){
@@ -17,8 +22,6 @@ function sortear(){
         while (sorteados.includes(numero)){
             numero = numeroAleatorio(de, ate);
         }
-
-
         sorteados.push(numero);
     }
     
